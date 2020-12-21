816a7b8877a56a8f822534b66f674370ce402331
deviana chynthia <deviana.c36@gmail.com>
2020-12-21 09:09:29 +0700

    Recycler View

--- app/src/main/java/com/example/project3u/MainActivity.java
+++ app/src/main/java/com/example/project3u/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.project3u;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +18,17 @@ public class MainActivity extends AppCompatActivity {
         //called object and create from class of java
         //Project gettingStartedProject = new Project("Getting Starter App","Our very first project, the default 'Hello World' App !", 5);
 
+        //Recycler view
+        RecyclerView list_object = findViewById(R.id.recycler_list_view_object);
+
+
         //Array
-        Project[] projects ={
-                new Project("Getting Starter App","Our very first project, the default 'Hello World' App !", R.drawable.getting_started),
-                new Project("Dev Quote","Second project, the simple quote", R.drawable.quote),
-                new Project("BMI Calculator","A Real Life working BMI calculator", R.drawable.calculator),
-                new Project("Iches Converter","A basic Inches calculator !", R.drawable.tape),
-                new Project("The Hungry Dev","List View menu of restaurant", R.drawable.hungry_developer)
-
-
+        Project[] projects = {
+                new Project("Getting Starter App", "Our very first project, the default 'Hello World' App !", R.drawable.getting_started),
+                new Project("Dev Quote", "Second project, the simple quote", R.drawable.quote),
+                new Project("BMI Calculator", "A Real Life working BMI calculator", R.drawable.calculator),
+                new Project("Iches Converter", "A basic Inches calculator !", R.drawable.tape),
+                new Project("The Hungry Dev", "List View menu of restaurant", R.drawable.hungry_developer)
         };
 
 
@@ -30,3 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
+//Ini cara pake listView
+//        ListView listView = findViewById(R.id.recyle_list_view);
+//
+//        String [] strings = {"One", "Two" , "Three"};
+//
+//        ArrayAdapter<String> adapter  = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,strings);
+//
+//        listView.setAdapter(adapter);
---
+++ app/src/main/java/com/example/project3u/ProjectAdapter.java
@@ -0,0 +1,78 @@
+//package com.example.project3u;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+////buat class khusus untuk adapter ke Recycler View nya nanti
+//public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder> {
+//
+//    private Project[] projects;
+//
+//    public ProjectAdapter(Project[] projects) {
+//        this.projects = projects;
+//
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        //return 0;
+//        //mengambil data sebanyak jumlah data array nya
+//        return projects.length;
+//    }
+//
+//
+//    @NonNull
+//    @Override
+//    public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//
+//        //layout akses
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_project, parent, false))
+//        ;
+//
+//        //return null;
+//        return new ProjectViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
+//        //Akses project view holder
+//        //apapun posisi nya ambil dari array nya biisa diakses
+//        holder.bind(projects[position]);
+//    }
+//
+//
+//    //public class another class di class
+//    static class ProjectViewHolder extends RecyclerView.ViewHolder {
+//        ImageView appImage;
+//        TextView appTitle;
+//        TextView appDesc;
+//
+//
+//        public ProjectViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            appImage = itemView.findViewById(R.id.image_view_project_icon);
+//            appTitle = itemView.findFocus(R.id.text_view_title_app);
+//            appDesc = itemView.findFocus(R.id.text_view_desc_app);
+//        }
+//
+//        public void bind(Project project) {
+//            //setting image dan tv
+//            appTitle.setText(project.name);
+//            appDesc.setText(project.description);
+//            appImage.setImageResource(project.image);
+//        }
+//    }
+//}
+
+
+
+
+
+
